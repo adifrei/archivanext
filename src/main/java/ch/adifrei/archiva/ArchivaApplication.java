d4e33c4475eafdf4ff4df155ac14646322308447
Adrian Frei <adrian.frei@trivadis.com>
2022-08-07 15:23:16 +0200

    Initial commit

---
+++ src/main/java/ch/adifrei/archiva/ArchivaApplication.java
@@ -0,0 +1,13 @@
+package ch.adifrei.archiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivaApplication.class, args);
+	}
+
+}
